8f4846195d9bd901f4bdc7d8c11328904a85d457
Valery Zubkov <bouddha@mail.ru>
2022-08-24 22:30:07 +0400

    homework number 2

---
+++ HOMEWORK_002/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void IplusJFillMatrix(int[,] matr)
+{
+    for(int i=0; i<matr.GetLength(0); i++)
+    {
+        for(int j=0; j<matr.GetLength(1); j++)
+        {
+            matr[i, j] = i + j;
+        }
+    }
+}
+void PrintMatrix(int[,] matr)
+{
+   for(int i=0; i<matr.GetLength(0); i++)
+    {
+        for(int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+Console.WriteLine("Введите количесво строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количесво столбцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[m,n];
+IplusJFillMatrix(matrix);
+PrintMatrix(matrix);
